9f9cdecf460e714dbb04a5840e12e4b2a6ba263c
Suresh Reddy Guntaka <sureshreddy.guntaka@gmail.com>
2017-09-15 23:10:34 +0530

    Gulp Clean task defined

--- gulp/config.js
+++
@@ -1,8 +0,0 @@
-var args = require('minimist')(process.argv.slice(2));
-var VERSION = args.version || require('../package.json').version;
-
-module.exports = {
-    version         : VERSION,
-    jsBaseFiles     : [],
-    jsFiles         : []
-}
